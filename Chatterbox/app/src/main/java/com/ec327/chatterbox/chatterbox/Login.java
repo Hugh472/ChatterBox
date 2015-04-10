d1ad2ed9f467ed054d540917f25023c5f12a679f
Joonho Han <joonhohan365@gmail.com>
2015-04-10 16:32:01 -0400

    Finished Chatterbox login screen

--- Chatterbox/app/src/main/java/com/ec327/chatterbox/chatterbox/Login.java
+++ Chatterbox/app/src/main/java/com/ec327/chatterbox/chatterbox/Login.java
@@ -1,7 +1,21 @@
 package com.ec327.chatterbox.chatterbox;
 
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
 /**
  * Created by Mason D. Hahn on 4/7/2015.
  */
-public class Login {
+public class Login extends Activity{
+
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        EditText et = (EditText) findViewById(R.id.login_nickname_input);
+        EditText et2 = (EditText) findViewById(R.id.login_password_input);
+
+        Button signIn = (Button) findViewById(R.id.login_signIn);
+        Button signUp = (Button) findViewById(R.id.login_signUp);
+    }
 }
--- Chatterbox/app/src/main/java/com/ec327/chatterbox/chatterbox/SignUp.java
+++ Chatterbox/app/src/main/java/com/ec327/chatterbox/chatterbox/SignUp.java
@@ -1,7 +1,9 @@
 package com.ec327.chatterbox.chatterbox;
 
+import android.app.Activity;
+
 /**
  * Created by Mason D. Hahn on 4/7/2015.
  */
-public class SignUp {
+public class SignUp extends Activity{
 }
